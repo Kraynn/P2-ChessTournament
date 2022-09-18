f7aabe14d351ea0a320b23d4a5f145ec5713fec5
Kraynn <75184473+Kraynn@users.noreply.github.com>
2022-09-18 17:49:28 +0100

    Ajout de la fonction update_ranking

--- controllers/create_player.py
+++ controllers/create_player.py
@@ -1,4 +1,4 @@
-from tinydb import TinyDB
+from tinydb import TinyDB, Query
 import views.v_add_player as vp
 from models.player import Player
 
@@ -28,5 +28,34 @@ def register_player():
         register_player()
     elif reponse == "o":
         register_player()
+    else:
+        pass
+
+
+def show_players_rank():
+    db = TinyDB("db.json")
+    players_table = db.table("Joueurs")
+    message = ''
+    players = players_table.all()
+    players.sort(key=lambda k: k['Classement'])
+    print("Liste des joueurs et de leur classement: \n")
+    for player in players:
+        message += f'{player["Classement"]} {player["Nom"]} {player["Prenom"]} \n'
+    print(message)
+
+
+def update_ranking():
+    db = TinyDB("db.json")
+    User = Query()
+    players_table = db.table("Joueurs")
+    show_players_rank()
+    player = input(" > Saissisez le nom de famille d'un joueur \n")
+    new_rank = int(input(f"Quel est le nouveau classement de {player} ? \n"))
+    players_table.upsert({"Classement": new_rank}, User.Nom == f"{player}")
+    choice = input("> Voulez-vous modifier le classement d'un autre joueur? (O/N) \n")
+    if choice == "O":
+        update_ranking()
+    elif choice == "o":
+        update_ranking()
     else:
         pass
